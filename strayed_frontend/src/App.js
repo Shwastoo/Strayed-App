5d4c8236a3f57c531dd3e4903f409bc0727f791c
politowska <politowska@student.agh.edu.pl>
2023-07-19 08:37:14 +0200

    Fixing small incompabilities

--- strayed_frontend/src/App.js
+++ strayed_frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+/*import React, { Component } from "react";
+import Modal from "./components/Modal";*/
 
 import logo from "./logo.svg";
 import "./App.css";
